60abe6fea62c535225cfe89a6587211e1694c27c
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-10-31 11:54:41 +0000

    wip

--- config-rules/a9n/a9n.py
+++ config-rules/a9n/a9n.py
@@ -1,10 +1,13 @@
 from rdklib import Evaluator, Evaluation, ConfigRule, ComplianceType
 import datetime
 import logging
-log = logging.getLogger()
+import json
+log = logging.getLogger("a9n")
+log.setLevel(logging.INFO)
 
 class a9n(ConfigRule):
     def evaluate_change(self, event, client_factory, configuration_item, valid_rule_parameters):
+        print("--- evaluate change ---")
         resource_type = configuration_item.get("resourceType")
         resource_id = configuration_item.get("resourceId")
         log.info("Evaluating change [%s] [%s]", resource_type, resource_id)
@@ -14,21 +17,23 @@ class a9n(ConfigRule):
         log.debug(str(event))
         evaluation = Evaluation(ComplianceType.COMPLIANT, resourceId=resource_id, resourceType=resource_type, annotation="a9n")
         evaluations = [evaluation]
-        return []
+        return evaluations
 
     def evaluate_periodic(self, event, client_factory, valid_rule_parameters):
-        resource_type = event.get("resourceType")
-        resource_id = event.get("resourceId")                                                                                                                            
-        log.info("Evaluating periodic rule [%s] [%s]", resource_type, resource_id)
+        print("--- evaluate periodic ---")
+        print(json.dumps(event, indent=4, sort_keys=True))                                                                                                                   
+        log.info("Evaluating periodic rule",)
         log.debug("valid client parameters")
         log.debug(str(valid_rule_parameters))
         log.debug("event")
         log.debug(str(event))
-        evaluation = Evaluation(ComplianceType.COMPLIANT, resourceId=resource_id, resourceType=resource_type, annotation="a9n")
-        evaluations = [evaluation]
+        # evaluation = Evaluation(ComplianceType.COMPLIANT, resourceId=resource_id, resourceType=resource_type, annotation="a9n")
+        # evaluations = [evaluation]
         return []
 
     def evaluate_parameters(self, rule_parameters):
+        print("--- evaluate parameters ---")
+        print(json.dumps(rule_parameters, indent=4, sort_keys=True))
         valid_rule_parameters = rule_parameters
         return valid_rule_parameters
 
@@ -37,6 +42,9 @@ class a9n(ConfigRule):
 # DO NOT MODIFY ANYTHING BELOW #
 ################################
 def lambda_handler(event, context):
+    print("--- event ---")
+    print(json.dumps(event, indent=4, sort_keys=True))
     my_rule = a9n()
-    evaluator = Evaluator(my_rule)
+    evaluator = Evaluator(my_rule, expected_resource_types=["ALL"]) 
+    # TODO
     return evaluator.handle(event, context)
--- config-rules/add-permissions.sh
+++ config-rules/add-permissions.sh
@@ -9,3 +9,26 @@ aws lambda add-permission \
     --action lambda:InvokeFunction \
     --statement-id config \
     --principal config.amazonaws.com
+
+ROLE_NAME=$(jq -r '.StackResources[] | select(.ResourceType == "AWS::IAM::Role") | .PhysicalResourceId' .lambda-resources.json)
+echo $ROLE_NAME
+
+RDK_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+echo $RDK_ACCOUNT_ID
+
+envsubst < trust-policy.env.json > .trust-policy.json
+cat .trust-policy.json
+
+A9N_ROLE_NAME="${ROLE_NAME}_a9n_role"
+echo $A9N_ROLE_NAME
+
+aws iam create-role \
+  --role-name $A9N_ROLE_NAME \
+  --assume-role-policy-document file://.trust-policy.json \
+  --description "Role with AWS_ConfigRole managed policy and trust relationship with AWS Config and /rdk/ path roles" | tee .a9n-role.json
+
+aws iam attach-role-policy \
+    --role-name "$A9N_ROLE_NAME" \
+    --policy-arn "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
+
+
